f826b76847b2f72850f839edb33c2d8849e27a69
Brandon Kwamou <brandonkwamou@gmail.com>
2024-01-17 15:25:15 +0100

    Working with external data

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
-    {
+    public function index(MicroPostRepository $posts): Response
+    {   
+        # CREATE
+        // $microPost1 = new MicroPost(); 
+        // $microPost1->setTitle('It comes from controller'); 
+        // $microPost1->setText('Hi!'); 
+        // $microPost1->setCreated(new DateTime()); 
+        # UPDATE
+        // $microPost = $posts->findOneBy(['title' => "It comes from controller	"]);   
+        // $microPost->setText('Just trying the update function'); 
+        # REMOVE
+        // $microPost = $posts->findOneBy(['title' => "It comes from controller"]); 
+        // $posts->remove($microPost, true); 
+
+        // dd($posts->findAll());
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
+        ]);
+
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response {
+        // dd($post);
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
         ]);
     }
 }
