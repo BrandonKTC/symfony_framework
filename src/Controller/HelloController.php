46141db106099e1a43471c35519359315a0da954
Brandon Kwamou <brandonkwamou@gmail.com>
2024-01-17 12:29:26 +0100

    unused file

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -10,13 +10,16 @@ class HelloController extends AbstractController
 
 {
     private array $messages = [
-        "Hello", "Hi", "Bye!"
+        ["message" => "Hello", "created" => "2023/12/12"],
+        ["message" => "Hi", "created" => "2023/10/12"],
+        ["message" => "Bye!", "created" => "2022/05/12"],
     ];
 
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): Response {
         return $this->render('hello/index.html.twig', [
-            'messages' =>  array_slice($this->messages, 0, $limit)
+            'messages' => $this->messages,
+            'limit' => $limit
         ]);
         // return new Response(implode(',', array_slice($this->messages, 0, $limit)));
     }
